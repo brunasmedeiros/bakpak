2d3741b7cb85e3e0e5631d4b3a076c446aef927d
Bruna Enos <bruna_bsm@icloud.com>
2017-02-25 22:14:26 -0600

    [fix] autocomplete to cities

--- public/search.js
+++ public/search.js
@@ -2,7 +2,11 @@ angular.module('bakpak.search', [])
 
 .controller('searchBar', function($scope, $http){
 
+  const options = {
+  types: ['(cities)'],
+  componentRestrictions: {country: "us"}
+  };
   const input = document.getElementById('input');
-  $scope.input = new google.maps.places.SearchBox(input);
+  $scope.input = new google.maps.places.Autocomplete(input, options);
   $scope.city;
 });
